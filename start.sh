ed53b5826baab0db2d646598fa1f4d7e2d006098
Andy <59578532+andyloox@users.noreply.github.com>
2024-09-01 17:07:58 +0300

    Update start.sh5

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@ clear
 apt install git
 rm -r /var/ps5host
 mkdir /var/ps5host
-mkdir /var/ps5host/PS5-Exploit-Host
 cd /var/ps5host
 folder="/var/ps5host/PS5-Exploit-Host"
 c_beginning_url="https://github.com/idlesauce/PS5-Exploit-Host.git"
 
 repo_url=$c_beginning_url
+git init
 if [ -d ${folder} ]
 then
   echo -e "\e[31m${folder} already exist!\e[39m"
